5fc067b0a802b3ea1a9ba5e7d40d1a1c72684d90
karanpulipati-adeptia <karan.pulipati@adeptia.com>
2026-08-01 16:28:24 +0530

    ci: trigger apply for fresh EKS 1.32 cluster

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -54,7 +54,7 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_readonly" {
 resource "aws_eks_cluster" "main" {
   name     = local.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.32"
+  version  = "1.32" # EKS 1.32
 
   vpc_config {
     subnet_ids = var.private_subnet_ids
