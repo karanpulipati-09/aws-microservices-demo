e6bad0582e5760d0fdff138ef5739de18776e715
karanpulipati-adeptia <karan.pulipati@adeptia.com>
2026-08-03 12:12:34 +0530

    fix: upgrade EKS nodes t3.micro → t3.small (more pod capacity)

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -76,7 +76,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = var.private_subnet_ids
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 2
